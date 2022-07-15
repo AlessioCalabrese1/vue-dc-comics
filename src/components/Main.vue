a5c3564cc5e20cee9433e99112ee8fdcb6f177e5
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-07-15 17:52:06 +0200

    Main: content

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,11 @@
 <template>
-  
+  <main>
+    <div class="content-container">
+        <div>
+            -->Content goes here<--
+        </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -8,6 +14,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "../styles/variables.scss";
 
+.content-container{
+    background-color: black;
+
+    div{
+        margin: 0 auto;
+        width: $elements-width;
+        font-weight: bold;
+        color: white;
+        font-size: 25px;
+        padding: 25px 0;
+    }
+}
 </style>
